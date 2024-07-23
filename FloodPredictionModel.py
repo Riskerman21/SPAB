466da825469f3ad5502f1187e4cb5f441756974f
Abdallah Azazy <abdallah33319@gmail.com>
2024-07-24 00:54:54 +0700

    and again

--- FloodPredictionModel.py
+++ FloodPredictionModel.py
@@ -19,19 +19,19 @@ for filename in os.listdir(directory):
     if os.path.isfile(file_path):
         # Open the Excel file to check the number of sheets
         excel_file = pd.ExcelFile(file_path)
-        
+
         # Check if the file has at least two sheets
         if len(excel_file.sheet_names) > 1:
             # Read the second sheet
             file = pd.read_excel(file_path, sheet_name=1)
-            
+
             # Get the date of the data
             filename_without_extension = filename.split('.')[0]
-            
+
             # Format date
             date_obj = datetime.strptime(filename_without_extension, "%Y%m%d")
             formatted_date = date_obj.strftime("%Y-%m-%d")
-            
+
             # Append to date list
             flooddata_dict[formatted_date] = file
 
