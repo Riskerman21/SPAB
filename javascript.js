124dc42470160cc54b107b4fe74ef7da93df0495
Bailey Stoodley <Bailey@bunnycup.com>
2024-07-23 18:29:41 +0700

    fixing style

--- javascript.js
+++ javascript.js
@@ -11,20 +11,16 @@ document.addEventListener('DOMContentLoaded', function() {
         { src: 'map.html', title: 'Day 7' }
     ];
 
-
     let currentIndex = 0;
     let intervalId;
 
     function updateMap() {
-        // Update the map title
         const titleContainer = document.getElementById('map-title-container');
         titleContainer.innerHTML = `<h2>${mapsData[currentIndex].title}</h2>`;
 
-        // Update the map iframe
         const mapContainer = document.getElementById('map-container');
         mapContainer.innerHTML = `<iframe src="${mapsData[currentIndex].src}" class="w-100" style="height: 75vh;"></iframe>`;
 
-        // Update index to loop through maps
         currentIndex = (currentIndex + 1) % mapsData.length;
     }
 
@@ -36,11 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
         clearInterval(intervalId);
     }
 
-    // Initial render
     updateMap();
     startUpdating();
 
-    // Pause button event listener
     document.getElementById('pause-button').addEventListener('click', function() {
         const button = this;
         if (button.textContent === 'Pause') {
