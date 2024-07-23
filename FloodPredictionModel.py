a97dfe46bae0095e2a41bcaea45f9352a0ef8b48
sidney1469 <sidneyneil04@outlook.com>
2024-07-24 01:02:10 +1000

    combined the two datasets

--- FloodPredictionModel.py
+++ FloodPredictionModel.py
@@ -1,31 +1,47 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
-
-#import data into panda datastructures
-rain_data = pd.read_csv('raindata/raindata2023-2024.csv')
-#flood_data = pd.read_excel('flooddata/')
-print(rain_data)
-#extract dates
-rain_dates = rain_data['datetime']
 import os
 
+# Import rain data into pandas dataframe
+rain_data = pd.read_csv('raindata/raindata2023-2024.csv')
+print(rain_data)
+
+# Extract dates from rain data
+rain_dates = list(rain_data['datetime'])
+
+# Initialize dictionary to hold flood data
 directory = 'flooddata/date/'
 flooddata_dict = {}
+
 # Loop through all files in the directory
 for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
     if os.path.isfile(file_path):
-        #open excel file
+        # Open excel file
         file = pd.read_excel(file_path)
-        #get the date of the data
-        file_path = file_path.split('/')[1]
-        filename = file_path.split('.')[0]
-        #format date
-        date_obj = datetime.strptime(filename, "%Y%m%d")
+        
+        # Get the date of the data
+        filename_without_extension = filename.split('.')[0]
+        
+        # Format date
+        date_obj = datetime.strptime(filename_without_extension, "%Y%m%d")
         formatted_date = date_obj.strftime("%d/%m/%Y")
-        #append to date list
-        flooddata_dict.update({formatted_date: file})
+        
+        # Append to date list
+        flooddata_dict[formatted_date] = file
 
-print(flooddata_dict)
+# Merge rain data with flood data based on dates
+for i in range(rain_data.shape[0]):
+    for n in range(len(rain_dates)):
+        rain_date = rain_dates[n]
+        for flood_date in flooddata_dict.keys():
+            if flood_date == rain_date:
+                flood_data = flooddata_dict[flood_date]
+                rain_data.loc[n, flood_data.columns] = flood_data.values[0]
+
+print(rain_data)
+
+
+
 
