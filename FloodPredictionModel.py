fbe0921f5defb5eecdf2c5b6ebf4a29317df3bf1
Bailey Stoodley <Bailey@bunnycup.com>
2024-07-24 10:26:06 +0700

    merged dataframe creation

--- FloodPredictionModel.py
+++ FloodPredictionModel.py
@@ -3,44 +3,33 @@ import pandas as pd
 from datetime import datetime
 import os
 
-# Import rain data into pandas dataframe
-rain_data = pd.read_csv('raindata/raindata2023-2024.csv')
-
-# Extract dates from rain data
-rain_dates = list(rain_data['datetime'])
-
-# Initialize dictionary to hold flood data
 directory = 'flooddata/date/'
-flooddata_dict = {}
+flooddata_list = []
 
-# Loop through all files in the directory
 for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
+
     if os.path.isfile(file_path):
-        # Open the Excel file to check the number of sheets
         excel_file = pd.ExcelFile(file_path)
-
-        # Check if the file has at least two sheets
         if len(excel_file.sheet_names) > 1:
-            # Read the second sheet
             file = pd.read_excel(file_path, sheet_name=1)
-
-            # Get the date of the data
             filename_without_extension = filename.split('.')[0]
 
-            # Format date
             date_obj = datetime.strptime(filename_without_extension, "%Y%m%d")
             formatted_date = date_obj.strftime("%Y-%m-%d")
+            file['Date'] = formatted_date
+            
+            flooddata_list.append(file)
 
-            # Append to date list
-            flooddata_dict[formatted_date] = file
+flooddata_df = pd.concat(flooddata_list, ignore_index=True)
 
-flood_dates = list(flooddata_dict.keys())
+rain_data = pd.read_csv('raindata/raindata2023-2024.csv')
 
-# Merge rain data with flood data based on dates
-for n in range(len(rain_dates)):
-    rain_date = rain_dates[n]
-    if rain_date in flood_dates:
-        for category in rain_data.columns:
-            if category != 'datetime':
-                flooddata_dict[rain_date] = pd.concat([flooddata_dict[rain_date], pd.DataFrame([rain_data[category]])])
+# Ensure date format consistency
+flooddata_df['Date'] = pd.to_datetime(flooddata_df['Date'])
+rain_data['datetime'] = pd.to_datetime(rain_data['datetime'])
+
+merged_df = flooddata_df.merge(rain_data, left_on='Date', right_on='datetime', how='left')
+
+print(merged_df)
+merged_df.to_excel('merged_data.xlsx', index=False)
