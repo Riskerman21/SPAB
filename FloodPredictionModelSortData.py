9daf56e0d0282746c07bd65d528a106683a9cfed
sidney1469 <sidneyneil04@outlook.com>
2024-07-24 15:41:39 +1000

    added formatting to long and lat

--- FloodPredictionModelSortData.py
+++ FloodPredictionModelSortData.py
@@ -5,4 +5,15 @@ data = pd.read_excel(file_path)
 
 # Create a set of tuples containing (lat, long)
 longitudes = set(zip(data['lat'], data['long']))
-print(longitudes)
+
+# Convert the set to a list
+longitude_list = list(longitudes)
+
+# Create a DataFrame from the list
+longitude_df = pd.DataFrame(longitude_list, columns=['lat', 'long'])
+
+# Save the DataFrame to an Excel file
+output_file_path = 'unique_longitudes.xlsx'
+longitude_df.to_excel(output_file_path, index=False)
+
+print(f"Data has been saved to {output_file_path}")
